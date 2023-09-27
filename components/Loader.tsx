e4fc37fd2eb7332c001ec435503306bd2cd103c7
Shiv Akash <shivakash2003@gmail.com>
2023-09-27 13:06:48 +0530

    feature: new loading screen

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { BeatLoader } from "react-spinners";
 
 type Props = {};
 
-export default function Loader({}: Props) {
+const Loader: React.FC<Props> = () => {
   return (
-    <header>
-      <div>Loading...</div>
-    </header>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 overflow-x-hidden">
+      <div className="fixed inset-0 flex justify-center items-center">
+        <BeatLoader size={25} color={"#ffffff"} loading={true} />
+      </div>
+    </div>
   );
-}
+};
+
+export default Loader;
