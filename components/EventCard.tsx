b459529b1be045fe1c7ca906f4abf360ac8cdcfb
Chu1503 <shivakash2003@gmail.com>
2024-01-26 22:37:23 +0530

    added events page

---
+++ components/EventCard.tsx
@@ -0,0 +1,49 @@
+// import creatoverse from "../images/creatoverse.png";
+// import illumina from "../images/illumina.png";
+// import coding_odyssey from "../images/coding_odyssey.png";
+// import subway_cyphers from "../images/subway_cyphers.png";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+type EventCardProps = {
+  imageUrl: string;
+  heading: string;
+  listItems: string[];
+};
+
+function EventCard({ imageUrl, heading, listItems }: EventCardProps) {
+  return (
+    <article
+      className="flex flex-col items-center space-y-7 flex-shrink-0 w-[250px] xl:w-[500px] snap-center 
+    p-10 bg-[rgba(52,52,52,0.5)] hover:bg-[rgba(52,52,52,1)] overflow-hidden "
+    >
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        transition={{ duration: 1.5 }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        viewport={{ once: true }}
+        className="w-40 h-40 bg-black xl:w-[200px] xl:h-[200px] object-cover object-center"
+        src={imageUrl}
+        alt={heading}
+      />
+
+      <div className="px-0 md:px-10">
+        <h3 className="text-2xl text-center font-bold pb-5">{heading}</h3>
+        <ul className="list-disc space-y-4 ml-5 text-lg">
+          {listItems.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </article>
+  );
+}
+
+export default EventCard;
--- components/Events.tsx
+++ components/Events.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { motion } from "framer-motion";
+import EventCard from "./EventCard";
+import creatoverse from "../images/creatoverse.png";
+import illumina from "../images/illumina.png";
+import coding_odyssey from "../images/coding_odyssey.png";
+import subway_cyphers from "../images/subway_cyphers.png";
 
 type Props = {};
 
@@ -15,11 +20,34 @@ export default function Events({}: Props) {
       transition={{
         duration: 1.5,
       }}
-      className="flex flex-col relative h-auto text-center max-w-7xl p-10 justify-evenly mx-auto items-center"
+      className="flex flex-col relative h-auto text-left max-w-7xl p-10 justify-evenly mx-auto items-center overflow-hidden md:text-row "
     >
-      <h2 className="pt-20 p-10 uppercase tracking-[20px] text-gray-500 text-3xl sm:text-4xl">
+      <h2 className="pt-20 p-10 uppercase tracking-[20px] text-gray-500 text-4xl sm:text-3xl">
         Events
       </h2>
+
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-none">
+        <EventCard
+          imageUrl={creatoverse.src}
+          heading="CREATOVERSE"
+          listItems={["Item 1", "Item 2", "Item 3"]}
+        />
+        <EventCard
+          imageUrl={illumina.src}
+          heading="ILLUMMINA"
+          listItems={["Item 1", "Item 2", "Item 3"]}
+        />
+        <EventCard
+          imageUrl={coding_odyssey.src}
+          heading="CODING ODYSSEY"
+          listItems={["Item 1", "Item 2", "Item 3"]}
+        />
+        <EventCard
+          imageUrl={subway_cyphers.src}
+          heading="SUBWAY CYPHERS"
+          listItems={["Item 1", "Item 2", "Item 3"]}
+        />
+      </div>
     </motion.div>
   );
 }
